24f44792c8e93bef6c3269390f9d89220adf25a0
Mahmud Hajiyev <mahmud.hajiyev@outlook.com>
2018-06-21 16:41:08 +0400

    Add project files.

---
+++ BookingAppStore/Models/Purchase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingAppStore.Models
+{
+    public class Purchase
+    {
+        public int PurchaseID { get; set; }
+
+        public string Purchaser { get; set; }
+
+        public string Address { get; set; }
+
+        public float BookId { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
---
+++ BookingAppStore/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BookingAppStore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Rapid Solutions Ltd.")]
+[assembly: AssemblyProduct("BookingAppStore")]
+[assembly: AssemblyCopyright("Copyright © Rapid Solutions Ltd. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("724b7847-d6c5-4496-a144-24ce54b6f6e9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
